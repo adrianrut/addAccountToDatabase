ab832d807c845884562b643ba06cd4423f48436e
N'Avalon Production <adrianrutkowskiarcgas@gmail.com>
2021-12-09 23:08:33 +0100

    abc

--- src/main/java/pl/rutkowski/addaccounttodatabase/Start.java
+++ src/main/java/pl/rutkowski/addaccounttodatabase/Start.java
@@ -1,19 +1,19 @@
 package pl.rutkowski.addaccounttodatabase;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequiredArgsConstructor
 @Component
 public class Start {
 
     private UserRepo userRepo;
 
-    @Autowired
-    public Start(UserRepo userRepo) {
-        this.userRepo = userRepo;
-    }
     @EventListener(ApplicationReadyEvent.class)
     public void run() {
         User user1 = new User("Suhy", "miszcz", "babochłop",Hobby.Sport);
---
+++ src/main/java/pl/rutkowski/addaccounttodatabase/UserDetails.java
@@ -0,0 +1,5 @@
+package pl.rutkowski.addaccounttodatabase;
+
+public class UserDetails {
+
+}
